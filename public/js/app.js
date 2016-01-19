78df95c0df6b1b526f938d836503fcd03ca9f01e
zezhipeng <372962347@qq.com>
2016-01-19 15:48:31 +0800

    done

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,66 @@
 /**
  * Created by root on 16-1-18.
  */
-angular.module("app", ["ngResource"])
-    .controller("rest", function ($scope, $resource) {
-        
-    })
+(function (angular) {
+    angular.module("app", ["ngResource"])
+        .config(['$resourceProvider', function ($resourceProvider) {
+            $resourceProvider.defaults.stripTrailingSlashes = false;
+        }])
+        .factory("restful", function ($resource) {
+            var restful = this
+            restful.user = $resource("/user/:name", {name: "@id"},
+                {
+                    getOneUser: {method: "GET"},
+                    getUsers: {method: "GET", params: {}, isArray: true},
+                    createUser: {method: "POST"},
+                    updateUser:{method:"PUT"},
+                    delUser:{method:"DELETE"}
+                }
+            );
+            return restful
+        })
+        .controller("restful", function ($scope, restful) {
+            var userInit =  function(){
+                restful
+                    .user
+                    .getUsers()
+                    .$promise
+                    .then(function (res) {
+                        console.log(res)
+                        $scope.users = res
+                    });
+            };
+            userInit();
+            $scope.login = function (user) {
+                restful
+                    .user
+                    .createUser(user)
+                    .$promise
+                    .then(function (res) {
+                        console.log(res)
+                        userInit();
+                    })
+            };
+            $scope.put =function(user){
+              console.log(user)
+                restful
+                    .user
+                    .updateUser(user)
+                    .$promise
+                    .then(function(res){
+                        userInit()
+                    })
+            };
+            $scope.del = function(user){
+                restful
+                    .user
+                    .delUser({name:user.name})
+                    .$promise
+                    .then(function(res){
+                        userInit()
+                    })
+            }
+
+        })
+
+})(window.angular);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
+var _ = require("lodash");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
+var users=[];
+
+router.get("/user",function(req,res){
+  res.json(users)
+});
+
+router.get("/user/:name",function(req,res){
+  //console.log(req.params);
+  res.json({_id:req.params})
+});
+
+router.post("/user",function(req,res){
+  console.log(req.body.name);
+  if(!_.find(users,{name:req.body.name})){
+    users.push(req.body);
+    res.json(req.body)
+  }
+  else{
+    res.json(null)
+  }
+});
+
+router.put("/user",function(req,res){
+  users= _.map(users,function(v){
+    if(v.name==req.body.name){
+      return v = req.body
+    }
+    return v
+  });
+  res.json(req.body)
+});
+
+router.delete("/user/:name",function(req,res){
+   //console.log(req.params);
+   users= _.filter(users,function(v){
+     return v.name!=req.params.name
+   });
+  res.json(req.params)
+});
 module.exports = router;
